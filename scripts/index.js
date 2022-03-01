3570396517702352ba85a3afb440d4dff29e524c
antaww <anta.pizzetta@gmail.com>
2022-03-01 17:53:25 +0100

    charalist animation while loading

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,30 @@
-const arrow = document.querySelector(".arrow")
+// const arrow = document.querySelector(".arrow")
+//
+// arrow.addEventListener("click", () => {
+//         console.log("arrow clicked")
+//     }
+// )
 
-arrow.addEventListener("click", () => {
-        console.log("arrow clicked")
+window.addEventListener("load", function () {
+    console.log("page loaded")
+
+    function isVisibleInViewport(element) {
+        if (element.offsetWidth || element.offsetHeight || element.getClientRects().length) {
+            const rect = element.getBoundingClientRect();
+            return rect.bottom > 0 && rect.right > 0 && rect.left < (window.innerWidth || document.documentElement.clientWidth) && rect.top < (window.innerHeight || document.documentElement.clientHeight);
+        }
+        return false;
     }
-)
+
+    function animateVisibleElements() {
+        const list = document.querySelectorAll('.chara_box');
+
+        list.forEach(item => {
+            if (isVisibleInViewport(item)) item.classList.add('animate');
+        });
+    }
+
+    document.addEventListener('scroll', animateVisibleElements);
+    animateVisibleElements();
+
+});
