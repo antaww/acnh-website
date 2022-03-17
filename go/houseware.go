703347b39a7bd4a5a9593a76211098cb4ecabd97
Bahsiik <o.mist1303@gmail.com>
2022-03-17 08:42:44 +0100

    add new attributes to houseware struct

--- go/houseware.go
+++ go/houseware.go
@@ -9,55 +9,6 @@ import (
 	"strings"
 )
 
-type test struct {
-	Variant             interface{} `json:"variant"`
-	BodyTitle           interface{} `json:"body-title"`
-	Pattern             interface{} `json:"pattern"`
-	PatternTitle        interface{} `json:"pattern-title"`
-	IsDIY               bool        `json:"isDIY"`
-	CanCustomizeBody    bool        `json:"canCustomizeBody"`
-	CanCustomizePattern bool        `json:"canCustomizePattern"`
-	KitCost             interface{} `json:"kit-cost"`
-	Color1              string      `json:"color-1"`
-	Color2              string      `json:"color-2"`
-	Size                string      `json:"size"`
-	Source              string      `json:"source"`
-	SourceDetail        string      `json:"source-detail"`
-	Version             string      `json:"version"`
-	HhaConcept1         string      `json:"hha-concept-1"`
-	HhaConcept2         interface{} `json:"hha-concept-2"`
-	HhaSeries           interface{} `json:"hha-series"`
-	HhaSet              interface{} `json:"hha-set"`
-	IsInteractive       bool        `json:"isInteractive"`
-	Tag                 string      `json:"tag"`
-	IsOutdoor           bool        `json:"isOutdoor"`
-	SpeakerType         interface{} `json:"speaker-type"`
-	LightingType        interface{} `json:"lighting-type"`
-	IsCatalog           bool        `json:"isCatalog"`
-	FileName            string      `json:"file-name"`
-	VariantId           interface{} `json:"variant-id"`
-	InternalId          int         `json:"internal-id"`
-	Name                struct {
-		NameUSen string `json:"name-USen"`
-		NameEUen string `json:"name-EUen"`
-		NameEUde string `json:"name-EUde"`
-		NameEUes string `json:"name-EUes"`
-		NameUSes string `json:"name-USes"`
-		NameEUfr string `json:"name-EUfr"`
-		NameUSfr string `json:"name-USfr"`
-		NameEUit string `json:"name-EUit"`
-		NameEUnl string `json:"name-EUnl"`
-		NameCNzh string `json:"name-CNzh"`
-		NameTWzh string `json:"name-TWzh"`
-		NameJPja string `json:"name-JPja"`
-		NameKRko string `json:"name-KRko"`
-		NameEUru string `json:"name-EUru"`
-	} `json:"name"`
-	BuyPrice  int    `json:"buy-price"`
-	SellPrice int    `json:"sell-price"`
-	ImageUri  string `json:"image_uri"`
-}
-
 type HousewareRawData struct {
 	URL               string `json:"url"`
 	Name              string `json:"name"`
@@ -111,10 +62,20 @@ type SimplifiedHouseware struct {
 		Price    int
 		Currency string
 	}
-	From    string
-	Note    string
-	Themes  []string
-	Version string
+	Availability []struct {
+		From string
+		Note string
+	}
+	Versions []struct {
+		ImageURL string
+		Colors   []string
+	}
+	Themes       []string
+	Category     string
+	ItemSeries   string
+	Tag          string
+	Sell         int
+	VersionAdded string
 }
 
 func toDataHouseware(housewaredata HousewareRawData) SimplifiedHouseware {
@@ -122,6 +83,7 @@ func toDataHouseware(housewaredata HousewareRawData) SimplifiedHouseware {
 	for i := range housewaredata.Variations {
 		images = append(images, housewaredata.Variations[i].ImageURL)
 	}
+
 	var buy []struct {
 		Price    int
 		Currency string
@@ -132,31 +94,53 @@ func toDataHouseware(housewaredata HousewareRawData) SimplifiedHouseware {
 	}
 	for i := range housewaredata.Buy {
 		data := buyData{Price: housewaredata.Buy[i].Price, Currency: housewaredata.Buy[i].Currency}
-		//buy[i].Price = housewaredata.Buy[i].Price
-		//buy[i].Currency = housewaredata.Buy[i].Currency
 		buy = append(buy, data)
 	}
-	var from string
-	for i := range housewaredata.Availability {
-		from = housewaredata.Availability[i].From
+
+	var availability []struct {
+		From string
+		Note string
 	}
-	var note string
-	for i := range housewaredata.Availability {
-		note = housewaredata.Availability[i].Note
+	type availabilityData struct {
+		From string
+		Note string
 	}
+	for i := range housewaredata.Availability {
+		data := availabilityData{From: housewaredata.Availability[i].From, Note: housewaredata.Availability[i].Note}
+		availability = append(availability, data)
+	}
+
+	var versions []struct {
+		ImageURL string
+		Colors   []string
+	}
+	type versionsData struct {
+		ImageURL string
+		Colors   []string
+	}
+	for i := range housewaredata.Variations {
+		data := versionsData{ImageURL: housewaredata.Variations[i].ImageURL, Colors: housewaredata.Variations[i].Colors}
+		versions = append(versions, data)
+	}
+
 	var themes []string
 	for i := range housewaredata.Themes {
 		themes = append(themes, housewaredata.Themes[i])
 	}
+
 	return SimplifiedHouseware{
 		Name:           housewaredata.Name,
 		NameSimplified: housewaredata.NameSimplified,
+		Category:       housewaredata.Category,
+		ItemSeries:     housewaredata.ItemSeries,
+		Tag:            housewaredata.Tag,
 		ImageURL:       images,
 		Buy:            buy,
-		From:           from,
-		Note:           note,
+		Availability:   availability,
 		Themes:         themes,
-		Version:        housewaredata.VersionAdded,
+		Versions:       versions,
+		Sell:           housewaredata.Sell,
+		VersionAdded:   housewaredata.VersionAdded,
 	}
 }
 
